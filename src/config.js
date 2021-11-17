4e541353e0dae3bea25455db79605fb44b84ce29
Pranav Shinde <46496160+pra9shinde@users.noreply.github.com>
2021-11-17 19:22:01 +0530

    static content path fix

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     SERVER_URL: "https://shindetter.herokuapp.com/graphql",
-    STATIC_FILES_URL: "https://shindetter.herokuapp.com/",
+    STATIC_FILES_URL: "https://shindetter.herokuapp.com",
 };
 
 export default config;
